6680acb1f759d39627b39a068bc9b202701a5648
fzqk <40433012+fzqk@users.noreply.github.com>
2018-11-19 22:48:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/bn/',
+    prefix: '/bjgc/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
