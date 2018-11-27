280ca2bf553c07014fee8adf4ff84092d32581fa
fzqk <40433012+fzqk@users.noreply.github.com>
2018-11-27 20:11:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/pkab/',
+    prefix: '/xpom/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
