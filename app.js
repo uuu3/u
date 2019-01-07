ed0a074e037fe7fe6d1c1484f90e423e33ce08a0
fzqk <40433012+fzqk@users.noreply.github.com>
2019-01-07 13:13:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/muiip/',
+    prefix: '/giz/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
