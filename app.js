e4fcca404f8246cba622314024bf054d86fd43ca
fzqk <40433012+fzqk@users.noreply.github.com>
2019-01-09 06:15:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/nf/',
+    prefix: '/bltoo/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
