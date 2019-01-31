eb948d2a869b6f16282cf8d46a9f5cbb6f4b8657
fzqk <40433012+fzqk@users.noreply.github.com>
2019-01-31 20:11:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/ezotv/',
+    prefix: '/xcgu/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
