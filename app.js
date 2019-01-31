df7f528ad54dfeb64be140e2117b04dfa4972f48
fzqk <40433012+fzqk@users.noreply.github.com>
2019-01-31 13:13:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/zdesr/',
+    prefix: '/ezotv/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
