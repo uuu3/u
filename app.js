98d20efc4c0c3c424e6e32da4dbf88e6394bbfae
fzqk <40433012+fzqk@users.noreply.github.com>
2019-02-24 06:15:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/tvwa/',
+    prefix: '/ng/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
