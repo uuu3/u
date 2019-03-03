a658c71a6e679b4b89ec82dfb9d9d96f566d12f0
fzqk <40433012+fzqk@users.noreply.github.com>
2019-03-03 20:11:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/hrsa/',
+    prefix: '/npfr/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
